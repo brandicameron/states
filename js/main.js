8cd396e3805fac5ff72330e893d5d2ceef6d6db3
Brandi Cameron <hello@brandicameron.com>
2021-07-02 13:26:01 -0500

    redesign

--- js/main.js
+++ js/main.js
@@ -1,96 +1,96 @@
 let counter = 0,
   shuffledStates,
-  correctStates = []
-const stateQuestionDisplay = document.querySelector('.state')
-const question = document.querySelector('.question')
-const gameOverGraphic = document.querySelector('.game-over')
+  correctStates = [];
+const stateQuestionDisplay = document.querySelector('.state');
+const question = document.querySelector('.question');
+const gameOverGraphic = document.querySelector('.game-over');
 
 function shuffle(array) {
-  // Big thanks to: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   let currentIndex = array.length,
     temporaryValue,
-    randomIndex
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
     // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
     // And swap it with the current element.
-    temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
-  return array
+  return array;
 }
 
 function gameQuestion() {
-  shuffledStates = shuffle(states)
-  stateQuestionDisplay.textContent = shuffledStates[0]
+  shuffledStates = shuffle(states);
+  stateQuestionDisplay.textContent = shuffledStates[0];
 }
 
 function trackClicks(e) {
   if (e.target.classList.contains('st0')) {
-    counter++
-    displayCounter()
+    counter++;
+    displayCounter();
     if (e.target.id.replace(/_/g, ' ') === stateQuestionDisplay.textContent) {
-      correctAnswer(e)
+      correctAnswer(e);
     } else {
-      e.target.setAttribute('class', 'incorrect')
+      e.target.setAttribute('class', 'incorrect');
       setTimeout(() => {
-        e.target.setAttribute('class', 'grey st0')
-      }, 100)
+        e.target.setAttribute('class', 'grey st0');
+      }, 100);
     }
   }
 }
 
 function correctAnswer(e) {
-  let correctSound = document.getElementById('correct-sound')
-  correctSound.currentTime = 0
-  correctSound.play()
-  e.target.setAttribute('class', 'correct')
-  correctStates.push(stateQuestionDisplay.textContent)
-  displayScore()
-  stateQuestionDisplay.textContent = shuffledStates[correctStates.length]
+  let correctSound = document.getElementById('correct-sound');
+  correctSound.currentTime = 0;
+  correctSound.play();
+  e.target.setAttribute('class', 'correct');
+  correctStates.push(stateQuestionDisplay.textContent);
+  displayScore();
+  stateQuestionDisplay.textContent = shuffledStates[correctStates.length];
 
   if (correctStates.length === 50) {
-    gameOver()
+    gameOver();
   }
 }
 
 function displayCounter() {
-  let clickDisplay = document.querySelector('.click-display')
-  clickDisplay.textContent = counter
+  let clickDisplay = document.querySelector('.click-display');
+  clickDisplay.textContent = counter;
 }
 
 function displayScore() {
-  let displayScore = document.querySelector('.score-display')
-  displayScore.textContent = correctStates.length
+  let displayScore = document.querySelector('.score-display');
+  displayScore.textContent = correctStates.length;
 }
 
 function gameOver() {
-  document.getElementById('cheer-sound').play()
-  gameOverGraphic.classList.add('grow')
-  question.style.visibility = 'hidden'
+  document.getElementById('cheer-sound').play();
+  gameOverGraphic.classList.add('grow');
+  question.style.visibility = 'hidden';
 }
 
 function restartGame() {
-  const allStates = document.querySelectorAll('.correct')
+  const allStates = document.querySelectorAll('.correct');
   allStates.forEach((state) => {
-    state.setAttribute('class', 'grey st0')
-  })
+    state.setAttribute('class', 'grey st0');
+  });
 
-  question.style.visibility = 'visible'
-  gameOverGraphic.classList.remove('grow')
-  counter = 0
-  correctStates = []
-  displayScore()
-  displayCounter()
-  gameQuestion()
+  question.style.visibility = 'visible';
+  gameOverGraphic.classList.remove('grow');
+  counter = 0;
+  correctStates = [];
+  displayScore();
+  displayCounter();
+  gameQuestion();
 }
 
-document.querySelector('#united-states').addEventListener('click', trackClicks)
-document.querySelector('.restart-btn').addEventListener('click', restartGame)
+document.querySelector('#united-states').addEventListener('click', trackClicks);
+document.querySelector('.restart-btn').addEventListener('click', restartGame);
 
-gameQuestion()
+gameQuestion();
