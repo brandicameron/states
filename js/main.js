47d073f5fe43e94f6bdb586ee411fed637d5b34b
Brandi Cameron <hello@brandicameron.com>
2021-07-24 14:48:44 -0500

    removed vibration code, broke on iOS

--- js/main.js
+++ js/main.js
@@ -37,7 +37,6 @@ function trackClicks(e) {
     if (e.target.id.replace(/_/g, ' ') === stateQuestionDisplay.textContent) {
       correctAnswer(e);
     } else {
-      navigator.vibrate(500);
       e.target.setAttribute('class', 'incorrect');
       setTimeout(() => {
         e.target.setAttribute('class', 'grey st0');
